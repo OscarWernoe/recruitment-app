c12b288453da0ffecc699fc5ff01f3c148b7c7fa
robinnilsson94 <rbn.nilsson@gmail.com>
2019-03-06 13:08:51 +0100

    Added more validations and updated the test class for SignUpRequest.

--- src/main/java/se/kth/iv1201/recruitment/domain/SignUpRequest.java
+++ src/main/java/se/kth/iv1201/recruitment/domain/SignUpRequest.java
@@ -7,28 +7,36 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+/**
+ * DTO containing user input validations for the sign up request.
+ */
 @Data
 public class SignUpRequest {
 
     @NotBlank
+    @Size(min = 2, max = 50)
     private String name;
 
     @NotBlank
+    @Size(min = 2, max = 50)
     private String surname;
 
     @Email
     @NotBlank
+    @Size(max = 50)
     private String email;
 
     @Pattern(regexp = "^[0-9]*$")
     @NotBlank
+    @Size(min = 10, max = 10)
     private String ssn;
 
     @NotBlank
+    @Size(max = 50)
     private String username;
 
     @NotBlank
-    @Size(min = 8)
+    @Size(min = 8, max = 100)
     private String password;
 
     public SignUpRequest(String name, String surname, String email, String ssn, String username, String password) {
--- src/test/java/se/kth/iv1201/recruitment/domain/SignUpRequestTest.java
+++ src/test/java/se/kth/iv1201/recruitment/domain/SignUpRequestTest.java
@@ -26,7 +26,7 @@ public class SignUpRequestTest {
 
     @Test
     public void validRequest() {
-        SignUpRequest validRequest = new SignUpRequest("Test", "Test", "test@test.com", "1234", "testUsername", "testPassword");
+        SignUpRequest validRequest = new SignUpRequest("Test", "Test", "test@test.com", "1234567891", "testUsername", "testPassword");
         Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(validRequest);
         assertThat(violations.size(), is(0));
     }
@@ -35,15 +35,30 @@ public class SignUpRequestTest {
     public void blankRequest() {
         SignUpRequest blankRequest = new SignUpRequest("", "", "", "", "", "");
         Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(blankRequest);
-        // password fires twice, both blank and too short, email fires on blank
-        assertThat(violations.size(), is(7));
+        assertThat(violations.size(), is(10));
     }
 
     @Test
-    public void invalidRequest() {
-        // test that invalid email, non-numeric ssn, and short password fires (3 violations)
-        SignUpRequest invalidRequest = new SignUpRequest("Test", "Test", "emailWithoutAtSymbol", "1234a", "testUsername", "short");
+    public void invalidRequestConcerningChar() {
+        // test that invalid email and non-numeric ssn fires 2 violations.
+        SignUpRequest invalidRequest = new SignUpRequest("Test", "Test", "emailWithoutAtSymbol", "123456789a", "testUsername", "mediumLength");
         Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(invalidRequest);
-        assertThat(violations.size(), is(3));
+        assertThat(violations.size(), is(2));
+    }
+
+    @Test
+    public void invalidRequestConcerningMin() {
+        // test that too short length of name, surname, ssn and password fires 4 violations.
+        SignUpRequest invalidRequest = new SignUpRequest("T", "V", "email@correct.com", "123456789", "testuser", "short");
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(invalidRequest);
+        assertThat(violations.size(), is(4));
+    }
+
+    @Test
+    public void invalidRequestConcerningMax() {
+        // test that too long length of name, surname, email, ssn, username and password fires 6 violations.
+        SignUpRequest invalidRequest = new SignUpRequest("TestTestTestTestTestTestTestTestTestTestTestTestTest", "TestTestTestTestTestTestTestTestTestTestTestTestTest", "email@correct.com.TestTestTestTestTestTestTestTestTestTestTestTestTest", "12345678910", "testuserTestTestTestTestTestTestTestTestTestTestTestTestTest", "UltraShortPasswordUltraShortPasswordUltraShortPasswordUltraShortPasswordUltraShortPasswordUltraShortPassword");
+        Set<ConstraintViolation<SignUpRequest>> violations = validator.validate(invalidRequest);
+        assertThat(violations.size(), is(6));
     }
 }
